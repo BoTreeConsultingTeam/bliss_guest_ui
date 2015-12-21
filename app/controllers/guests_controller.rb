c070d3f8604e67d844a7b4fc41bd77b2a71e6fb2
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2015-12-21 15:03:10 +0530

    Add a method that call API to search an entity with an API and returns a response either containing that entity or message "Not found". Then a popup will be display containg the appropriate message.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,8 +5,24 @@ class GuestsController < ApplicationController
   def login
   end
 
-  def seach_user
+  def search_user
     @search = Guests::Search.new(params)
     @guest_list = @search.call
+    if @guest_list.present?
+      if @guest_list["entity"] == "Not found"
+        @message = t("login.failure_message")
+      else
+        if @guest_list["entity"]["person"].present?
+          if @guest_list["entity"]["person"]["banned"]
+            @message = t("login.banned_match_message")
+          else
+            @message = t("login.success_message")
+          end
+        end
+        end
+      end
+    respond_to do |format|
+      format.js
+    end
   end
 end
